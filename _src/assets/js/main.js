554da4b40e09c9d79b25058d12c03c25cd5b40c7
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-11-21 19:42:29 +0100

    Write new task and add to task list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('>> Ready :)');
+const addBtn = document.querySelector('.add-btn');
+const newCardSection = document.querySelector('.new-card');
+const newTaskInput = document.querySelector('.new-task__input');
+const newTaskBtn = document.querySelector('.new-task__btn');
+const taskList = document.querySelector('.card__list');
+
+function showNewTask() {
+  newCardSection.classList.toggle('hidden');
+}
+
+function writeNewTask() {
+  const newTaskName = newTaskInput.value;
+  taskList.innerHTML += `<li>${newTaskName}</li>`;
+  newCardSection.classList.toggle('hidden');
+}
+
+addBtn.addEventListener('click', showNewTask);
+newTaskBtn.addEventListener('click', writeNewTask);
